331aeeb507163a212bbef86eb710cc4ba312fed7
Soumargha bhattacharjee <soumargha17@gmail.com>
2024-08-25 19:06:02 +0530

    check 3

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
   const submitHandler = async (values) => {
     try {
       setLoading(true);
-      const { data } = await axios.post('/users/login', values);
+      const { data } = await axios.post('https://expensify-production.vercel.app/api/v1/users/login', values);
       setLoading(false);
       message.success('Login successful');
       localStorage.setItem('user', JSON.stringify({ ...data.user, password: '' }));
